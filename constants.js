1012c8143bc65e6d13b83ac0cb586caea8d3b822
Mahdi MP <mahdi.mp@gmail.com>
2022-02-17 18:54:25 +0330

    Add Create wallet service

---
+++ constants.js
@@ -0,0 +1,8 @@
+const requestConfigs = {
+  Headers: {
+    'Content-Type': 'application/json',
+  }
+}
+const apiAddress = 'https://api.s0.t.hmny.io'
+
+module.exports = {requestConfigs, apiAddress}
--- routes/index.js
+++ routes/index.js
@@ -1,30 +1,92 @@
-const { Account } = require('@harmony-js/account');
-const { HttpProvider, Messenger } = require('@harmony-js/network');
-const { ChainID, ChainType } = require('@harmony-js/utils');
-const axios = require('axios').default;
+const { Account, Wallet } = require('@harmony-js/account')
+const { HttpProvider, Messenger } = require('@harmony-js/network')
+const { ChainID, ChainType } = require('@harmony-js/utils')
+const {apiAddress, requestConfigs} = require('../constants')
+const axios = require('axios').default
+const bip39 = require('bip39')
 
-var express = require('express');
-var router = express.Router();
+const {
+  generatePrivateKey,
+  getPubkeyFromPrivateKey,
+} = require('@harmony-js/crypto');
 
-router.get('/account-balance/:address', function (req, res, next) {
-  const { address } = req.params;
+
+
+var express = require('express')
+var router = express.Router()
+
+
+router.get('/account/balance/:address', function (req, res, next) {
+  const { address } = req.params
+
   const account = new Account(
     address,
     new Messenger(
-      new HttpProvider('https://api.s0.b.hmny.io'),
+      new HttpProvider(apiAddress),
       ChainType.Harmony,
     ),
     ChainID.HmyTestnet,
-  );
+  )
   account.getBalance().then(response => {
-    res.json(response);
-  });
+    res.json(response)
+  })
 })
 
-router.get('/wallet-balance/:address', function (req, res, next) {
-  const { address } = req.params;
+router.get('/wallet/new', function (req, res, next) {
+  const privateKey = generatePrivateKey();
+  const publicKey = getPubkeyFromPrivateKey(privateKey);
 
-  const apiAddress = 'https://api.harmony.one/';
+  const wallet = new Wallet(
+    new Messenger(
+      new HttpProvider(apiAddress),
+      ChainType.Harmony,
+      ChainID.RootstockMainnet,
+    ),
+  );
+  
+  const account = wallet.addByPrivateKey(privateKey)
+  account.privateKey
+
+  return res.json({
+    accountAddress: account.address,
+    privateKey : account.privateKey,
+    publicKey : account.publicKey,
+    accountBech32Address : account.bech32Address,
+  })
+})
+
+router.get('/wallet/in-transactions/:address', function (req, res, next) {
+  const { address } = req.params
+
+  const payload = {
+    "jsonrpc": "2.0",
+    "id": 1,
+    "method": "hmyv2_getTransactionsHistory",
+    "params": [
+      {
+        "address": address,
+        "pageIndex": 0,
+        "pageSize": 10,
+        "fullTx": true,
+        "txType": "RECEIVED",
+        "order": "DESC"
+      }
+    ]
+  }
+
+  axios.post(apiAddress, payload, requestConfigs)
+    .then(function (response) {
+      const { transactions } = response.data.result || []
+      res.json({
+        result : {
+          transactions
+        }
+      })
+    })
+})
+router.get('/wallet/balance/:address', function (req, res, next) {
+  const { address } = req.params
+
   const payload = {
     "id": "1",
     "jsonrpc": "2.0",
@@ -32,23 +94,18 @@ router.get('/wallet-balance/:address', function (req, res, next) {
     "params": [
       address
     ]
-  };
-  const requestConfig = {
-    Headers: {
-      'Content-Type': 'application/json',
-    }
   }
 
-  axios.post(apiAddress, payload, requestConfig).then(response => {
-    const { result } = response.data;
-    const balance = (+result > 0) ? (+result / 1000000000000000000) : 0;
+  axios.post(apiAddress, payload, requestConfigs).then(response => {
+    const { result } = response.data
+    const balance = (+result > 0) ? (+result / 1000000000000000000) : 0
 
     res.json({
       result: {
         balance: balance
       }
-    });
-  });
+    })
+  })
 })
 
-module.exports = router;
+module.exports = router
